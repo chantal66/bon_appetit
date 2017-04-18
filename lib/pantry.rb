54095ee2ef856b8a0dfc8a225ddd5404012f7cc0
Chantal Justamond <cjustamm66@me.com>
2017-04-18 11:11:46 -0600

    restock is not updating I choked

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -3,13 +3,21 @@ require_relative 'recipe'
 class Pantry
 
   attr_reader :stock
+  attr_accessor :amount
+              
               
   def initialize
     @stock = {}
-    # @ingredient = Recipe.new
+    @amount = []
   end
 
-  def stock_check("Cheese")
+  def stock_check(ingredient)
+    @amount << stock.values.join.to_i
+    @amount.reduce(:+)
+  
+  end
 
+  def restock(ingredient, amount)
+    stock[ingredient] = amount
   end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -13,27 +13,35 @@ class PantryTest < Minitest::Test
 
   def test_it_returns_a_hash
     pantry = Pantry.new
-    result = pantry.stock
     
-    assert result.is_a? Hash
+    assert ({}), pantry
   end
 
-  def test_it_can_check_the_stock
-
+  def test_it_can_it_check_the_stock
     pantry = Pantry.new
-    ingredient = Recipe.new('Cheese')
-    ingredient.add_ingredient('Cheese', 0) 
-    # binding.pry
- 
-    assert_equal 0, pantry.stock_check("Cheese")
-  end
+
+    assert_equal 0, pantry.stock_check('Cheese')
+   end
 
   def test_it_can_restock_an_item
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+
+  def test_can_keep_track_of_ingredients
     skip
     pantry = Pantry.new
     pantry.restock("Cheese", 10)
+    pantry.restock('Cheese', 20)
+    # binding.pry
+    assert_equal 30, pantry.stock_check('Cheese')
+  end
 
-    assert_equal 10, pantry.stock_check("Cheese")
+  def test_can_receive_recipe_ingredients
+    pantry
+    
   end
 end
 
